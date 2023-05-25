7c7810cdca0655decd11a43bea90df397e35c0a5
So Hoan Hua <huasohoan@gmail.com>
2023-05-25 12:24:30 -0700

    sleep form working, saved

--- ghi/src/Sleep/SleepForm.js
+++ ghi/src/Sleep/SleepForm.js
@@ -55,7 +55,7 @@ function SleepForm() {
     }
 
     return (
-
+      <div className="container">
         <div className="row">
           <div className="offset-3 col-6">
             <div className="shadow p-4 mt-4">
@@ -63,12 +63,12 @@ function SleepForm() {
               <form onSubmit={handleSubmit} id="create-sleep-form">
 
                 <div className="form-floating mb-3">
-                  <input onChange={handleDateChange} placeholder="Date" required type="date" name="date" id="date" className="form-control" value={date}/>
                   <label htmlFor="date">Date</label>
+                  <input onChange={handleDateChange} placeholder="Date" required type="date" name="date" id="date" className="form-control" value={date}/>
                 </div>
                 <div className="form-floating mb-3">
-                  <input onChange={handleHoursChange} placeholder="Hours" required type="number" step="any" name="hours" id="hours" className="form-control" value={hours}/>
                   <label htmlFor="hours">Hours</label>
+                  <input onChange={handleHoursChange} placeholder="Hours" required type="number" step="any" name="hours" id="hours" className="form-control" value={hours}/>
                 </div>
                 <div className="mb-3">
                   <select onChange={handleQualityChange} required name="quality" id="quality" className="form-select" value={quality}>
@@ -85,7 +85,7 @@ function SleepForm() {
             </div>
           </div>
         </div>
-
+      </div>
     );
 }
 export default SleepForm
