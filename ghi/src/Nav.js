24bfed625ea95aed577b8dd8b62fccfc11a8d628
So Hoan Hua <huasohoan@gmail.com>
2023-05-26 23:12:24 -0700

    log in and signup button

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -51,8 +51,34 @@ const Nav = () => {
                     </>
                 ) : (
                     <>
-                        <NavLink to="/login">Login</NavLink>
-                        <NavLink to="/signup">Sign up</NavLink>
+                        <a
+                          className="group relative inline-block focus:outline-none focus:ring"
+                          href="/login"
+                        >
+                          <span
+                            className="absolute inset-0 translate-x-1.5 translate-y-1.5 bg-yellow-300 transition-transform group-hover:translate-y-0 group-hover:translate-x-0"
+                          ></span>
+
+                          <span
+                            className="relative inline-block border-2 border-current px-8 py-3 text-sm font-bold uppercase tracking-widest text-black group-active:text-opacity-75"
+                          >
+                            Log In
+                          </span>
+                        </a>
+                        <a
+                          className="group relative inline-block focus:outline-none focus:ring"
+                          href="/signup"
+                        >
+                          <span
+                            className="absolute inset-0 translate-x-1.5 translate-y-1.5 bg-yellow-300 transition-transform group-hover:translate-y-0 group-hover:translate-x-0"
+                          ></span>
+
+                          <span
+                            className="relative inline-block border-2 border-current px-8 py-3 text-sm font-bold uppercase tracking-widest text-black group-active:text-opacity-75"
+                          >
+                            Sign Up
+                          </span>
+                        </a>
                     </>
                 )}
             </div>
